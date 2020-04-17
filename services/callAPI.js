772cda441ee62582365b1c3c71853ddf9510f098
iamanoopc <mailto.anoopc@gmail.com>
2020-04-17 10:14:59 +0800

    fixed api url

--- services/callAPI.js
+++ services/callAPI.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export default {
   async getProjects() {
-    const res = await axios.get('https://pledge-camp-backend.herokuapp.com/projects');
+    const res = await axios.get('http://localhost:8000/projects');
     return res.data;
   },
   async postFilters(paramsObj) {
     // console.log('posted', paramsObj);
-    const res = await axios.post('https://pledge-camp-backend.herokuapp.com/filters', paramsObj);
+    const res = await axios.post('http://localhost:8000/filters', paramsObj);
     return res.data;
   },
 };
